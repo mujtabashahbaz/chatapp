74dc23780f9b918ffb5793985e1abbb4476a80f8
Mujtaba Shahbaz <mujtaba@Mujtabas-MacBook-Pro.local>
2024-10-04 20:31:01 +0500

    First

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
